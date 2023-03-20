6082870a7c4d3aa467878d0a5687454b233499d1
EvilMuffin23 <119830397+EvilMuffin23@users.noreply.github.com>
2023-03-20 02:56:06 -0400

    Update order.php

--- order.php
+++ order.php
@@ -1,6 +1,12 @@
 <?php 
 
 function insertOrder($orderId, $dIssued, $dReceived, $totPrice, $payment, $oUId, $oTId,$rTId, $connect){
+    $dIssued = strtotime($dIssued);
+    $dReceived = strtotime($dReceived);
+    $dIssued = date('Y-m-d', $dIssued);
+    $dReceived = date('Y-m-d', $dReceived);
+    echo $dIssued;
+    echo $dReceived;
     $insertTrip = "INSERT INTO ORDERS
     (ORDER_ID, DATE_ISSUED, DATE_RECEIVED, TOTAL_PRICE, PAYMENT_CODE, USER_ID, TRIP_ID, RECEIPT_ID) 
     VALUES 

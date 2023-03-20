638733f4d8752aed43447274779d826e660e2810
alina-tariq <alina.tariq@ryerson.ca>
2023-03-20 00:22:34 -0400

    removed php code at top of file which makes the app act up

--- loginPage.php
+++ loginPage.php
@@ -1,11 +1,3 @@
-<?php
-session_start();
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: mainpage.php");
-    exit;
-}
-?>
-
 <div class="container">
        <div class="row d_flex">
             <p>Username</p>

1228c91c883b654a424493022ddcc57620aa2b08
EvilMuffin23 <119830397+EvilMuffin23@users.noreply.github.com>
2023-03-19 21:49:26 -0400

    Update user.php

--- user.php
+++ user.php
@@ -1,10 +1,7 @@
 <?php
 
 function insertUser($uId, $uName, $uLogin, $phone, $uAddress, $uCity, $uProvince, $uPost, $uPassword, $uBalance, $uAcc, $uEmail, $connect){
-    $insertUser = "INSERT INTO USERS 
-                (USER_ID, UNAME, PHONE, EMAIL, UADDRESS, CITY, PROVINCE, POSTAL_CODE, LOGIN_ID, UPASSWORD, BALANCE, ACCOUNT_TYPE) 
-                VALUES 
-                ($uId, $uName, $phone, $uEmail, $uAddress, $uCity, $uProvince, $uPost, $uLogin, $uPassword, $uBalance, $uAcc);";
+    $insertUser = "INSERT INTO USERS (USER_ID, UNAME, PHONE, EMAIL, UADDRESS, CITY, PROVINCE, POSTAL_CODE, LOGIN_ID, UPASSWORD, BALANCE, ACCOUNT_TYPE) VALUES ($uId, '$uName', $phone, '$uEmail', '$uAddress', '$uCity', '$uProvince', '$uPost', '$uLogin', '$uPassword', $uBalance, $uAcc);";
 
     try {
         $result = mysqli_query($connect, $insertUser);

80db53e6d96ffb4c09878dca0200a5196546dc9d
alina-tariq <alina.tariq@ryerson.ca>
2023-04-05 02:55:49 -0400

    refactored search

--- functions/search.php
+++ functions/search.php
@@ -1,29 +1,47 @@
 <?php include('connInfo.php');
 
+class Search {
+    public $connect;
+    public $id;
+    public $orderType;
+
+    function __construct($id, $orderType) {
+        $this->connect = connect();
+        $this->id = $id;
+        $this->orderType = $orderType;
+    }
+
+    function searchOrder() {
+        switch ($this->orderType) {
+            case 'userId':
+                $searchQuery = "SELECT * FROM ORDERS WHERE user_id=$this->id;";
+                break;
+            case 'orderId':
+                $searchQuery = "SELECT * FROM ORDERS WHERE order_id=$this->id;";
+                break;
+        }
+    
+        try {
+            $result = mysqli_query($this->connect, $searchQuery);
+            $rows = [];
+            while($row = $result->fetch_assoc()){
+                $rows[] = $row;
+            }
+            echo json_encode($rows);
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
+    }
+
+}
+
 $connect = connect();
 
 $id = $_POST["id"];
 $type = $_POST["type"];
 
-switch ($type){
-    case 'userId':
-        $searchQuery = "SELECT * FROM ORDERS WHERE user_id=$id;";
-        break;
-    case 'orderId':
-        $searchQuery = "SELECT * FROM ORDERS WHERE order_id=$id;";
-        break;
-}
+$search = new Search($id, $type);
+$search->searchOrder();
 
-
-try {
-    $result = mysqli_query($connect, $searchQuery);
-    $rows = [];
-    while($row = $result->fetch_assoc()){
-        $rows[] = $row;
-    }
-    echo json_encode($rows);
-} catch (Exception $ex) {
-    echo $ex->getMessage();
-}
 
 ?>

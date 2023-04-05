fb2825dae1e27630ea57095c3f8e4ed856fa7f05
alina-tariq <alina.tariq@ryerson.ca>
2023-04-05 02:53:05 -0400

    refactored delete

--- functions/delete.php
+++ functions/delete.php
@@ -1,38 +1,52 @@
 <?php include('connInfo.php');
-
 mysqli_report(MYSQLI_REPORT_ALL);
-$connect = connect();
+class Delete {
+    public $id;
+    public $tableSelected;
+    public $connect;
+
+    function __construct($id, $tableSelected) {
+        $this->connect = connect();
+        $this->id = $id;
+        $this->tableSelected = $tableSelected;
+    }
+
+    function deleteValue() {
+        switch ($this->tableSelected) {
+            case 'items':
+                $deleteQuery = "DELETE FROM $this->tableSelected WHERE item_id=$this->id";
+                break;
+            case 'users':
+                $deleteQuery = "DELETE FROM $this->tableSelected WHERE user_id=$this->id";
+                break;
+            case 'trucks':
+                $deleteQuery = "DELETE FROM $this->tableSelected WHERE truck_id=$this->id";
+                break;  
+            case 'trips':
+                $deleteQuery = "DELETE FROM $this->tableSelected WHERE trip_id=$this->id";
+                break;
+            case 'shopping':
+                $deleteQuery = "DELETE FROM $this->tableSelected WHERE receipt_id=$this->id";
+                break;
+            case 'orders':
+                $deleteQuery = "DELETE FROM $this->tableSelected WHERE order_id=$this->id";
+                break;
+        }
+
+        try {
+            $result = mysqli_query($this->connect, $deleteQuery);
+            echo "Value Removed\n";
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
+    }
+
+}
 
 $id = $_POST["id"];
 $tableSelected = $_POST["tableSelect"];
 
-switch ($tableSelected){
-    case 'items':
-        $deleteQuery = "DELETE FROM $tableSelected WHERE item_id=$id";
-        break;
-    case 'users':
-        $deleteQuery = "DELETE FROM $tableSelected WHERE user_id=$id";
-        break;
-    case 'trucks':
-        $deleteQuery = "DELETE FROM $tableSelected WHERE truck_id=$id";
-        break;  
-    case 'trips':
-        $deleteQuery = "DELETE FROM $tableSelected WHERE trip_id=$id";
-        break;
-    case 'shopping':
-        $deleteQuery = "DELETE FROM $tableSelected WHERE receipt_id=$id";
-        break;
-    case 'orders':
-        $deleteQuery = "DELETE FROM $tableSelected WHERE order_id=$id";
-        break;
-}
-
-
-try {
-    $result = mysqli_query($connect, $deleteQuery);
-    echo "value removed\n";
-} catch (Exception $ex) {
-    echo $ex->getMessage();
-}
+$deleteVal = new Delete($id, $tableSelected);
+$deleteVal->deleteValue();
 
 ?>
